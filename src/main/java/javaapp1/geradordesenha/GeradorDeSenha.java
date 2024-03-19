8736c8ddd0e60f018071fbd1417e0ced1f18c141
assisDaniel <116357551+assisDaniel@users.noreply.github.com>
2024-03-19 10:47:43 -0300

    Estrutura básica do projeto

---
+++ src/main/java/javaapp1/geradordesenha/GeradorDeSenha.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package javaapp1.geradordesenha;
+
+/**
+ *
+ */
+public class GeradorDeSenha {
+
+    public static void main(String[] args) {
+        TelaPrincipal frame= new TelaPrincipal();
+        frame.setVisible(true);
+    }
+}
